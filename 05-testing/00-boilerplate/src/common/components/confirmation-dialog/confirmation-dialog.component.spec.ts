a88f8349307ad1782842672e101e09741f105b00
Ana Gomez <anamgomezdelgado@gmail.com>
2020-09-18 10:53:57 +0200

    add component test initial

---
+++ 05-testing/00-boilerplate/src/common/components/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+import { render } from '@testing-library/react';
+
+describe('Confirmation dialog component spec', () => {
+  it('should display dialog when isOpen is true', () => {
+    // Asset
+    // Act
+    // Assert
+  });
+
+  it('should not display dialog when isOpen is false', () => {
+    // Asset
+    // Act
+    // Assert
+  });
+});
