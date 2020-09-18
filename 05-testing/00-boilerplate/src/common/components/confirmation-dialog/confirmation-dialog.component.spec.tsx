0c64c5307286c8b2c7c590317574f2bdd1f52384
Ana Gomez <anamgomezdelgado@gmail.com>
2020-09-18 19:42:13 +0200

    add component test fix

---
+++ 05-testing/00-boilerplate/src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -0,0 +1,102 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+
+describe('Confirmation dialog component spec', () => {
+  it('should display dialog when isOpen is true', () => {
+    // Asset
+    const props = {
+      isOpen: true,
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+      title: 'Test title',
+      labels: {
+        closeButton: 'Test close button',
+        acceptButton: 'Test accept button',
+      },
+    };
+
+    // Act
+    render(<ConfirmationDialogComponent {...props} />);
+
+    const dialogElement = screen.getByRole('dialog');
+
+    // Assert
+    expect(dialogElement).toBeInTheDocument();
+  });
+
+  it('should not display dialog when isOpen is false', () => {
+    // Asset
+    const props = {
+      isOpen: false,
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+      title: 'Test title',
+      labels: {
+        closeButton: 'Test close button',
+        acceptButton: 'Test accept button',
+      },
+    };
+
+    // Act
+    render(<ConfirmationDialogComponent {...props} />);
+
+    const dialogElement = screen.queryByRole('dialog');
+
+    // Assert
+    expect(dialogElement).toEqual(null);
+  });
+
+  it('should call onAccept property when it clicks on Accept button', () => {
+    // Asset
+    const props = {
+      isOpen: true,
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+      title: 'Test title',
+      labels: {
+        closeButton: 'Test close button',
+        acceptButton: 'Test accept button',
+      },
+    };
+
+    // Act
+    render(<ConfirmationDialogComponent {...props} />);
+
+    const acceptButtonElement = screen.getByRole('button', {
+      name: props.labels.acceptButton,
+    });
+    userEvent.click(acceptButtonElement);
+
+    // Assert
+    expect(acceptButtonElement).toBeInTheDocument();
+    expect(props.onAccept).toHaveBeenCalled();
+  });
+
+  it('should call onClose property when it clicks on Cancel button', () => {
+    // Asset
+    const props = {
+      isOpen: true,
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+      title: 'Test title',
+      labels: {
+        closeButton: 'Test close button',
+        acceptButton: 'Test accept button',
+      },
+    };
+
+    // Act
+    render(<ConfirmationDialogComponent {...props} />);
+
+    const closeButtonElement = screen.getByRole('button', {
+      name: props.labels.closeButton,
+    });
+    userEvent.click(closeButtonElement);
+
+    // Assert
+    expect(closeButtonElement).toBeInTheDocument();
+    expect(props.onClose).toHaveBeenCalled();
+  });
+});
